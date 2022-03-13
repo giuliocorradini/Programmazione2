b621821e3258985597c0ee4a0c9d7af380ab919a
giuliocorradini <39673120+giuliocorradini@users.noreply.github.com>
2022-03-13 21:10:08 +0100

    Soluzione generale con un numero variabile di lampioni

--- 2.Bitwise/lampioni.cc
+++ 2.Bitwise/lampioni.cc
@@ -17,9 +17,9 @@ L Stampa lista lampioni funzionanti
 K Controllo risparmio energetico
 T Termina
 
-VERSIONE A 8 LAMPIONI
+SOLUZIONE GENERALE
 
-(c) Giulio Corradini - 2021
+(c) Giulio Corradini - 2022
 */
 
 #include <iostream>
@@ -40,29 +40,48 @@ enum stato_t {
     RIPARATO
 };
 
+struct lampioni_t {
+    unsigned int stato;
+    const int num;
+};
+
 /*
  *  Aggiorna lo stato della fila andando a marcare un lampione come guasto o funzionante.
  *
- *  @param stato_lampioni Reference a un bit vector che rappresenta lo stato dei lampioni.
+ *  @param lampioni Reference alla struct che rappresenta lo stato dei lampioni.
  *  @param lampione Numero del lampione di cui si vuole cambiare lo stato.
  *  @param stato Valore della enum che indica se il lampione è guasto (0) o riparato (1)
  */
-void aggiorna_stato(unsigned int &stato_lampioni, unsigned int lampione, enum stato_t stato) {
-    lampione &= 0xff;
+void aggiorna_stato(struct lampioni_t &lampioni, unsigned int lampione, enum stato_t stato) {
+    unsigned int validation_mask = (1 << lampioni.num) - 1;
 
-    stato_lampioni &= ~(1 << lampione);     //pulisci il bit lampione-esimo
-    stato_lampioni |= stato << lampione;    //imposta se stato=1
+    lampioni.stato &= ~(1 << lampione) & validation_mask;       //pulisci il bit lampione-esimo
+    lampioni.stato |= (stato << lampione) & validation_mask;    //imposta se stato=1
 }
 
-bool risparmio_energetico(unsigned int &stato_lampioni) {
-    return (stato_lampioni & 0x77) == 0x77;
+bool risparmio_energetico(struct lampioni_t &lampioni) {
+    unsigned int even_mask =
+        (1 << lampioni.num) - 1 & 0x55555555;                   //0x55 = b01010101, bit in pos. pari
+
+    return (lampioni.stato & even_mask) == even_mask;
 }
 
 int main() {
+    const int NUMERO_LAMPIONI = 20;
+    static_assert(NUMERO_LAMPIONI > 0 && NUMERO_LAMPIONI < 32, "Imposta un numero di lampioni compreso tra 1 e 31");
+    //  Disattivare il flag -Werror quando si usano 31 lampioni, altrimenti viene segnalato un
+    //  possibile overflow quando si genera la maschera per i bit abilitati     --> (1<<num) - 1
+    //      - shifting a sinistra più della dimensione del tipo int                 (1 << 32)
+    //
+    //  Il caso con 32 lampioni non funziona, per come sono generate le maschere in modo da
+    //  coprire dinamicamente tutti i casi.
 
-    unsigned int stato_lampioni = 0xff;     //8 lampioni funzionanti
-    unsigned int lampione;
+    struct lampioni_t lampioni = {
+        .stato = (1 << NUMERO_LAMPIONI) - 1,
+        .num = NUMERO_LAMPIONI,
+    };
 
+    unsigned int lamp;
     char scelta;
 
     do {
@@ -72,27 +91,37 @@ int main() {
         switch(scelta) {
             case 'G':
                 cout << "Che lampione si è guastato? ";
-                cin >> lampione;
-                aggiorna_stato(stato_lampioni, lampione, GUASTO);
+                cin >> lamp;
+                aggiorna_stato(lampioni, lamp, GUASTO);
                 break;
 
             case 'R':
                 cout << "Che lampione hai riparato? ";
-                cin >> lampione;
-                aggiorna_stato(stato_lampioni, lampione, RIPARATO);
+                cin >> lamp;
+                aggiorna_stato(lampioni, lamp, RIPARATO);
                 break;
 
             case 'L':
-                cout << "7 - - - - - - 0" << endl;
-
-                for(int i=7; i>=0; i--)
-                    cout << ((stato_lampioni & (0x01 << i)) >> i) << ' ';
-                
+                //Stampa "righello"
+                for(int i=lampioni.num - 1; i>=0; i--) {
+                    cout << ' ';
+                    if(i % 8 == 0) cout << i;
+                    else cout << '-';
+                }
                 cout << endl;
+
+                //Stampa lampioni
+                for(int i=lampioni.num - 1; i>=0; i--) {
+                    cout << ' ' << ((lampioni.stato & (1 << i)) >> i);
+                    if(i>=16 && i%8==0)
+                        cout << ' ';
+                }
+                cout << endl;
+
                 break;
 
             case 'K':
-                if(risparmio_energetico(stato_lampioni))
+                if(risparmio_energetico(lampioni))
                     cout << "I lampioni possono funzionare in risparmio energetico." << endl;
                 else
                     cout << "Risparmio energetico non consentito." << endl;
