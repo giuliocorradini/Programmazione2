22a19d4aebf5339202efad8dabdb1bd901b68395
giuliocorradini <39673120+giuliocorradini@users.noreply.github.com>
2022-03-14 12:32:35 +0100

    Tracce esercizi

---
+++ 3.Debugging/memoria.cpp
@@ -0,0 +1,39 @@
+/*
+ * memoria.cpp
+ *
+ *  Created on: 28 Feb 2020
+ *      Author: lbedogni
+ */
+
+#include <iostream>
+
+using namespace std;
+
+// Esercizio
+/* Non si puo cambiare il tipo di ritorno di miafunzione
+ * Occorre visualizzare il valore 5 nella stampa finale
+ */
+void miafunzione(int var) {
+	var = 5;
+}
+
+int main() {
+	int miaVariabile;
+	miafunzione(miaVariabile);
+	cout << miaVariabile << endl;
+}
+
+
+
+// Soluzione
+/*
+void miafunzione(int& var) {
+	var = 5;
+}
+
+int main() {
+	int* miaVariabile = (int*)malloc(sizeof(int));
+	miafunzione(*miaVariabile);
+	cout << *miaVariabile << endl;
+}
+*/
