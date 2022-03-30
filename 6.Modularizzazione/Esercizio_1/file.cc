14eea3dafa00101bbfa1be1ceeb55b83ef7f0188
giuliocorradini <39673120+giuliocorradini@users.noreply.github.com>
2022-03-30 11:15:12 +0200

    Esercizio linking incrociato

---
+++ 6.Modularizzazione/Esercizio_1/file.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+extern int a;
+
+void fun() {
+    cout << a;
+}
---
+++ 6.Modularizzazione/Esercizio_1/main.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int a;
+void fun();
+
+int main() {
+
+    cin >> a;
+
+    fun();
+
+    return 0;
+}
