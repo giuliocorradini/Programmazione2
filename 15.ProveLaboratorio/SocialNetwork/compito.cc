a5287e7657b49597aee4156da320b7f35932fede
giuliocorradini <39673120+giuliocorradini@users.noreply.github.com>
2022-06-16 15:14:45 +0200

    Initial commit for SocialNetwork test

---
+++ 15.ProveLaboratorio/SocialNetwork/compito.cc
@@ -0,0 +1,8 @@
+/*
+ * Punto 1: Creare un progetto (e il corrispondente makefile) per l’acquisizione
+ * del grafo. Il progetto include il modulo “grafo” e il file compito.cc. Il main
+ * nel file compito.cc deve contenere il codice per l’acquisizione del grafo
+ * orientato contenuto nel file graph quindi richiamare la procedura stampa(graph)
+ * da codificare nel file compito.cc che stampa gli archi del grafo.
+*/
+
