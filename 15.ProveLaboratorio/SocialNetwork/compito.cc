cc3cf62614f378dd54c591ae6b30fd1d3a58785b
giuliocorradini <39673120+giuliocorradini@users.noreply.github.com>
2022-06-16 15:43:36 +0200

    Esercizio 2

--- 15.ProveLaboratorio/SocialNetwork/compito.cc
+++ 15.ProveLaboratorio/SocialNetwork/compito.cc
@@ -1,14 +1,7 @@
-/*
- * Punto 1: Creare un progetto (e il corrispondente makefile) per l’acquisizione
- * del grafo. Il progetto include il modulo “grafo” e il file compito.cc. Il main
- * nel file compito.cc deve contenere il codice per l’acquisizione del grafo
- * orientato contenuto nel file graph quindi richiamare la procedura stampa(graph)
- * da codificare nel file compito.cc che stampa gli archi del grafo.
-*/
-
 #include <iostream>
 #include <fstream>
 #include "grafi.h"
+#include "node.h"
 using namespace std;
 
 graph parser() {
@@ -24,25 +17,60 @@ graph parser() {
         int from, to;
         in >> from >> to;
 
-        add_arc(g, from, to, 1);
+        if(in)
+            add_arc(g, from, to, 1);
     }
 
     in.close();
     return g;
 }
 
-void stampa(graph g) {
+node *parser_nodi(int n) {
+    node *nodes = new node[n];
+
+    ifstream in("node");
+
+    char trash;
+
+    for(int i=0; i<n && in; i++) {
+        in.getline(nodes[i].cont, 80);
+        in >> nodes[i].tipo;
+        in >> noskipws >> trash;
+    }
+
+    in.close();
+
+    return nodes;
+}
+
+void stampa(graph g, node *nodi) {
     for(int i=1; i<=g.dim; i++) {
         adj_list neigh = get_adjlist(g, i);
 
         while(neigh) {
-            cout << i << " " << get_adjnode(neigh) << endl;
+            node *n1, *n2;
+            n1 = &nodi[i-1];
+            n2 = &nodi[get_adjnode(neigh) - 1];
+
+            cout << n1->cont << " ";
+            if(n1->tipo == 'U' && n2->tipo == 'U')
+                cout << "FOLLOW";
+            else if (n1->tipo == 'U' && n2->tipo == 'T')
+                cout << "LIKE";
+            else if (n1->tipo == 'T' && n2->tipo == 'U')
+                cout << "OWNER";
+
+            cout << " " << n2->cont << endl;
+
             neigh = get_nextadj(neigh);
         }
+        cout << endl;
     }
 }
 
 int main() {
     graph g = parser();
-    stampa(g);
+    node *nodi = parser_nodi(g.dim);
+
+    stampa(g, nodi);
 }
