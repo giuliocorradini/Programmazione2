59ca62852b7c0d0c178af127d00ae289434be538
giuliocorradini <39673120+giuliocorradini@users.noreply.github.com>
2022-06-16 15:22:25 +0200

    Esercizio 1

--- 15.ProveLaboratorio/SocialNetwork/compito.cc
+++ 15.ProveLaboratorio/SocialNetwork/compito.cc
@@ -6,3 +6,43 @@
  * da codificare nel file compito.cc che stampa gli archi del grafo.
 */
 
+#include <iostream>
+#include <fstream>
+#include "grafi.h"
+using namespace std;
+
+graph parser() {
+    ifstream in("graph");
+    graph g;
+
+    int nodes;
+    in >> nodes;
+
+    g = new_graph(nodes);
+
+    while(in) {
+        int from, to;
+        in >> from >> to;
+
+        add_arc(g, from, to, 1);
+    }
+
+    in.close();
+    return g;
+}
+
+void stampa(graph g) {
+    for(int i=1; i<=g.dim; i++) {
+        adj_list neigh = get_adjlist(g, i);
+
+        while(neigh) {
+            cout << i << " " << get_adjnode(neigh) << endl;
+            neigh = get_nextadj(neigh);
+        }
+    }
+}
+
+int main() {
+    graph g = parser();
+    stampa(g);
+}
