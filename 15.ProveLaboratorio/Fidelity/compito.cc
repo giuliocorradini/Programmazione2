cbbb8927cfb10ba0d22f7d1eb0ddc22ee6e4c365
giuliocorradini <39673120+giuliocorradini@users.noreply.github.com>
2022-06-19 19:22:11 +0200

    Esercizio 2.b

--- 15.ProveLaboratorio/Fidelity/compito.cc
+++ 15.ProveLaboratorio/Fidelity/compito.cc
@@ -4,6 +4,28 @@
 #include "bst.h"
 using namespace std;
 
+/**
+ *  Calcola il totale dei punti delle carte con codice compreso tra inf e sup nel bst.
+ *  @param t - Struttura che rappresenta il bst
+ *  @param inf - estremo inferiore dei codici
+ *  @param sup - estremo superiore dei codici 
+ */
+int totalePunti(bst t, tipo_key inf, tipo_key sup) {
+    if(!t)
+        return 0;
+
+    if(get_key(t) >= inf && get_key(t) <= sup)
+        return get_value(t).punti + totalePunti(get_left(t), inf, sup) + totalePunti(get_right(t), inf, sup);
+    
+    else if(get_key(t) > sup)
+        return totalePunti(get_left(t), inf, sup);
+    
+    else if(get_key(t) < inf)
+        return totalePunti(get_right(t), inf, sup);
+
+    return 0;
+}
+
 
 int main() {
     bst carte = nullptr;
@@ -32,6 +54,14 @@ int main() {
     }
 
     print_values(carte);
+
+    int inf, sup;
+    cout << "Estremo inferiore: ";
+    cin >> inf;
+    cout << "Estremo superiore: ";
+    cin >> sup;
+
+    cout << "Totale dei punti: " << totalePunti(carte, inf, sup) << endl;
 
     return 0;
 }
